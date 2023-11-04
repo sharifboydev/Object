eae3c47f5e27c63343c42184a83caba75648c1ac
sharifboy <143162316+sharifboydev@users.noreply.github.com>
2023-11-04 17:06:36 +0500

    added new code

--- objects.py
+++ objects.py
@@ -1,23 +1,32 @@
-class Talaba:
-    def __init__(self, ism, familiya, tyil):
-        self.ism = ism
-        self.familiya = familiya
-        self.tyil = tyil
-        self.bosqich = 1
-
-    def get_info(self):
-        return f"{self.ism} {self.familiya}. {self.bosqich}-bosqich talabasi"
-
-    def set_bosqich(self, bosqich):
-        self.bosqich = bosqich
-
-    def get_update(self):
-        self.bosqich += 1
-
-
-talaba1 = Talaba("Alijon", "Valiyev", 2000)
-# talaba1.set_bosqich(3)
+# class Talaba:
+#     def __init__(self, ism, familiya, tyil):
+#         self.ism = ism
+#         self.familiya = familiya
+#         self.tyil = tyil
+#         self.bosqich = 1
+#
+#     def get_info(self):
+#         return f"{self.ism} {self.familiya}. {self.bosqich}-bosqich talabasi"
+#
+#     def set_bosqich(self, bosqich):
+#         self.bosqich = bosqich
+#
+#     def get_update(self):
+#         self.bosqich += 1
+#
+#
+# talaba1 = Talaba("Alijon", "Valiyev", 2000)
+# talaba1.get_update()
 # print(talaba1.get_info())
-
-# talaba1.bosqich = 2
-# print(talaba1.bosqich)
+#
+# talaba1.get_update()
+# print(talaba1.get_info())
+#
+# talaba1.get_update()
+# print(talaba1.get_info())
+#
+# # talaba1.set_bosqich(3)
+# # print(talaba1.get_info())
+#
+# # talaba1.bosqich = 2
+# # print(talaba1.bosqich)
