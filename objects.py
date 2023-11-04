e566f290922b2ec5f7fff767accddd93609bbfc0
sharifboy <143162316+sharifboydev@users.noreply.github.com>
2023-11-04 16:48:20 +0500

    changes code

--- objects.py
+++ objects.py
@@ -6,8 +6,18 @@ class Talaba:
         self.bosqich = 1
 
     def get_info(self):
-        return f"{self.ism} {self.familiya}. {self.bosqich} bosqich talabasi"
+        return f"{self.ism} {self.familiya}. {self.bosqich}-bosqich talabasi"
+
+    def set_bosqich(self, bosqich):
+        self.bosqich = bosqich
+
+    def get_update(self):
+        self.bosqich += 1
 
 
 talaba1 = Talaba("Alijon", "Valiyev", 2000)
-print(talaba1.get_info())
+# talaba1.set_bosqich(3)
+# print(talaba1.get_info())
+
+# talaba1.bosqich = 2
+# print(talaba1.bosqich)
