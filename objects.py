abc14ea2fbf6424822f859beeba78725d507884f
sharifboy <143162316+sharifboydev@users.noreply.github.com>
2023-11-04 17:28:22 +0500

    added new code and create new class

--- objects.py
+++ objects.py
@@ -1,20 +1,50 @@
-# class Talaba:
-#     def __init__(self, ism, familiya, tyil):
-#         self.ism = ism
-#         self.familiya = familiya
-#         self.tyil = tyil
-#         self.bosqich = 1
-#
-#     def get_info(self):
-#         return f"{self.ism} {self.familiya}. {self.bosqich}-bosqich talabasi"
-#
-#     def set_bosqich(self, bosqich):
-#         self.bosqich = bosqich
-#
-#     def get_update(self):
-#         self.bosqich += 1
-#
-#
+class Talaba:
+    def __init__(self, ism, familiya, tyil):
+        self.ism = ism
+        self.familiya = familiya
+        self.tyil = tyil
+        self.bosqich = 1
+
+    def get_info(self):
+        return f"{self.ism} {self.familiya}. {self.bosqich}-bosqich talabasi"
+
+    def set_bosqich(self, bosqich):
+        self.bosqich = bosqich
+
+    def get_update(self):
+        self.bosqich += 1
+
+
+class Fan:
+    def __init__(self, nomi):
+        self.nomi = nomi
+        self.talabalar_soni = 0
+        self.talabalar = []
+
+    def add_student(self, talaba):
+        self.talabalar.append(talaba)
+        self.talabalar_soni += 1
+
+    def get_stdents(self):
+        return [talaba1.get_info() for talaba in self.talabalar]
+
+
+matematika = Fan("Oliy matematika")
+talaba1 = Talaba("Alijon", "Valiyev", 2000)
+talaba2 = Talaba("Hasan", "Alimov", 2001)
+talaba3 = Talaba("Akrom", "Boriyev", 2002)
+
+matematika.add_student(talaba1)
+matematika.add_student(talaba2)
+matematika.add_student(talaba3)
+
+mat_talabalar = matematika.get_stdents()
+print(mat_talabalar)
+
+# print(matematika.talabalar_soni)
+# print(matematika.talabalar)
+
+
 # talaba1 = Talaba("Alijon", "Valiyev", 2000)
 # talaba1.get_update()
 # print(talaba1.get_info())
