d7f61a2386a6d6720ec46c773bc479eed62875ec
sharifboy <143162316+sharifboydev@users.noreply.github.com>
2023-11-04 16:22:48 +0500

    create object pyton code

---
+++ objects.py
@@ -0,0 +1,9 @@
+class Talaba:
+    def __init__(self, ism, familiya, tyil):
+        self.ism = ism
+        self.familiya = familiya
+        self.tyil = tyil
+        self.bosqich = 1
+
+    def get_info(self):
+        return f"{self.ism} {self.familiya}. {self.bosqich} bosqich talabasi"
